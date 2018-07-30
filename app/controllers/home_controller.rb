e39d9fbc60369079a9ffe6552f8006d19c83d3bf
SUKItheFox <mari.michelson@gmail.com>
2018-07-30 17:43:48 +0300

    Connect to Data API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+
+  	require 'net/http'
+  	require 'json'
+
+  	@url = 'https://api.coinmarketcap.com/v1/ticker/'
+  	@uri = URI(@url)
+  	@response = Net::HTTP.get(@uri)
+  	@coins = JSON.parse(@response)
+  	@my_coins = ["BTC", "XRP", "ETH",]
   end
 end
